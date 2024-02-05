d05768884fbe049dbdf3e3fb63dcf117c9223d89
doorkiPr <boumarafchakib7@gmail.com>
2024-02-05 05:03:17 +0100

    Test: add test case for placing ships in the gameboard

--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -1,4 +1,5 @@
 import Gameboard from "../modules/Gameboard";
+import Ship from "../modules/Ship";
 
 test("returns the correct gameboard name", () => {
   expect(new Gameboard("player1").getName()).toBe("player1");
@@ -13,4 +14,13 @@ test("returns the correct number of cells in the gameBoard array", () => {
     cellCount += row.length;
   });
   expect(cellCount).toBe(100);
+});
+
+test("returns ship when placed in the gameboard", () => {
+  const testGameboard = new Gameboard("testing");
+  const myShip = new Ship("Carrier");
+
+  testGameboard.placeShip(myShip, 1, 2);
+
+  expect(testGameboard.getGameboardArray()[1][2]).toBe(myShip);
 });
