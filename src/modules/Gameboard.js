16058ec30eb68dc9229d2b54145ff42629001b37
doorkiPr <boumarafchakib7@gmail.com>
2024-02-06 12:14:41 +0100

    Feat: add logic that places ships on the vertical Axis

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 export default function Gameboard(name) {
   const gameBoardArray = [];
   for (let y = 0; y < 10; y += 1) {
@@ -5,13 +6,21 @@ export default function Gameboard(name) {
   }
   const getName = () => name.toString();
   const getGameboardArray = () => gameBoardArray;
-  function placeShip(ship, y, x) {
+
+  function placeShip(ship, y, x, axis = "horizontal") {
     if (gameBoardArray[y][x]) return;
+    if (axis === "vertical") {
+      for (let i = 0; i < ship.getLength(); i += 1) {
+        gameBoardArray[y + i].splice(x, 1, ship); // itterate through each row , replace element with an index of X by ship object
+      }
+      return;
+    }
     if (gameBoardArray[y][x + ship.getLength() - 1] !== null) return; // if ship spans across a cell that is out of limits or contains another ship don't place it
     for (let i = 0; i < ship.getLength(); i += 1) {
-      gameBoardArray[y].splice(x + i, 1, ship); // spans ship across the gameboard cells depedning on it's length
+      gameBoardArray[y].splice(x + i, 1, ship); // select correct row, itterate through each cell changing the x index replacing each one by ship object
     }
   }
+
   return {
     getGameboardArray,
     getName,
