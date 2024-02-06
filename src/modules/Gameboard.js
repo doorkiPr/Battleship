faf63ddf54b8c5eaabbafb0e6344e3da1192805a
doorkiPr <boumarafchakib7@gmail.com>
2024-02-06 05:24:01 +0100

    Feat: add logic to prevent adding ships if it spans across another ship or if theres no space to place in the gameboard array

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -7,6 +7,7 @@ export default function Gameboard(name) {
   const getGameboardArray = () => gameBoardArray;
   function placeShip(ship, y, x) {
     if (gameBoardArray[y][x]) return;
+    if (gameBoardArray[y][x + ship.getLength() - 1] !== null) return; // if ship spans across a cell that is out of limits or contains another ship don't place it
     for (let i = 0; i < ship.getLength(); i += 1) {
       gameBoardArray[y].splice(x + i, 1, ship); // spans ship across the gameboard cells depedning on it's length
     }
