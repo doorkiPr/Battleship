39a75d53011ab1daf9659af5b204291438cbe5b4
doorkiPr <boumarafchakib7@gmail.com>
2024-02-06 13:08:49 +0100

    Feat: add logic that prevents placing a ship if one of the cells it spans across isn't empty

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -9,14 +9,16 @@ export default function Gameboard(name) {
 
   function placeShip(ship, y, x, axis = "horizontal") {
     if (gameBoardArray[y][x]) return;
+
     if (axis === "vertical") {
       if (gameBoardArray[y + ship.getLength() - 1] === undefined) return; // if ship spans across a cell that is out of limits don't place it
-
+      if (gameBoardArray[y + ship.getLength() - 1][x] !== null) return; // if ship spans across a cell that is not empty don't place it
       for (let i = 0; i < ship.getLength(); i += 1) {
         gameBoardArray[y + i].splice(x, 1, ship); // itterate through each row , replace element with an index of X by ship object
       }
       return;
     }
+
     if (gameBoardArray[y][x + ship.getLength() - 1] !== null) return; // if ship spans across a cell that is out of limits or contains another ship don't place it
     for (let i = 0; i < ship.getLength(); i += 1) {
       gameBoardArray[y].splice(x + i, 1, ship); // select correct row, itterate through each cell changing the x index replacing each one by ship object
