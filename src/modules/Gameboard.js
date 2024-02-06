a5ec09496542d57355200b29b8354fc21aa5f430
doorkiPr <boumarafchakib7@gmail.com>
2024-02-06 20:18:30 +0100

    Feat: add areAllSunk method and logic to check if all ships have sunked

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -2,6 +2,7 @@
 export default function Gameboard(name) {
   const gameBoardArray = [];
   const receivedCoordinatesArray = [];
+  const placedShips = [];
   for (let y = 0; y < 10; y += 1) {
     gameBoardArray.push(new Array(10).fill(null));
   }
@@ -16,6 +17,7 @@ export default function Gameboard(name) {
       if (gameBoardArray[y + ship.getLength() - 1][x] !== null) return; // if ship spans across a cell that is not empty don't place it
       for (let i = 0; i < ship.getLength(); i += 1) {
         gameBoardArray[y + i].splice(x, 1, ship); // itterate through each row , replace element with an index of X by ship object
+        placedShips.push(ship);
       }
       return;
     }
@@ -23,6 +25,7 @@ export default function Gameboard(name) {
     if (gameBoardArray[y][x + ship.getLength() - 1] !== null) return; // if ship spans across a cell that is out of limits or contains another ship don't place it
     for (let i = 0; i < ship.getLength(); i += 1) {
       gameBoardArray[y].splice(x + i, 1, ship); // select correct row, itterate through each cell changing the x index replacing each one by ship object
+      placedShips.push(ship);
     }
   }
 
@@ -34,10 +37,15 @@ export default function Gameboard(name) {
     if (gameBoardArray[y][x] !== "missed") gameBoardArray[y][x].hit();
   }
 
+  function areAllSunk() {
+    return placedShips.every((ship) => ship.isSunk() === true);
+  }
+
   return {
     getGameboardArray,
     getName,
     placeShip,
     receiveAttack,
+    areAllSunk,
   };
 }
