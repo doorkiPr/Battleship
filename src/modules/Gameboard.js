6c641b2dadbf3a59b7d9058bf5d133aaff4b560b
doorkiPr <boumarafchakib7@gmail.com>
2024-02-06 18:35:33 +0100

    Feat: add receiveAttack method

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -9,23 +9,32 @@ export default function Gameboard(name) {
 
   function placeShip(ship, y, x, axis = "horizontal") {
     if (gameBoardArray[y][x]) return;
+
     if (axis === "vertical") {
       if (gameBoardArray[y + ship.getLength() - 1] === undefined) return; // if ship spans across a cell that is out of limits don't place it
-
+      if (gameBoardArray[y + ship.getLength() - 1][x] !== null) return; // if ship spans across a cell that is not empty don't place it
       for (let i = 0; i < ship.getLength(); i += 1) {
         gameBoardArray[y + i].splice(x, 1, ship); // itterate through each row , replace element with an index of X by ship object
       }
       return;
     }
+
     if (gameBoardArray[y][x + ship.getLength() - 1] !== null) return; // if ship spans across a cell that is out of limits or contains another ship don't place it
     for (let i = 0; i < ship.getLength(); i += 1) {
       gameBoardArray[y].splice(x + i, 1, ship); // select correct row, itterate through each cell changing the x index replacing each one by ship object
     }
   }
 
+  function receiveAttack(y, x) {
+    if (gameBoardArray[y][x] === "missed") return;
+    if (!gameBoardArray[y][x]) gameBoardArray[y].splice(x, 1, "missed");
+    if (gameBoardArray[y][x]) gameBoardArray[y][x].hit();
+  }
+
   return {
     getGameboardArray,
     getName,
     placeShip,
+    receiveAttack,
   };
 }
