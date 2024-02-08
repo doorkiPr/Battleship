7422c20e4b39a170f982e2197fcb29c522a3ff26
doorkiPr <boumarafchakib7@gmail.com>
2024-02-08 21:59:45 +0100

    Feat: add logic for placeShip to return true or false if ship placement is valid or not

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -9,23 +9,24 @@ export default function Gameboard(name) {
   const getName = () => name.toString();
   const getGameboardArray = () => gameBoardArray;
   function placeShip(ship, y, x, axis = "horizontal") {
-    if (gameBoardArray[y][x]) return;
+    if (gameBoardArray[y][x]) return false;
 
     if (axis === "vertical") {
-      if (gameBoardArray[y + ship.getLength() - 1] === undefined) return; // if ship spans across a cell that is out of limits don't place it
-      if (gameBoardArray[y + ship.getLength() - 1][x] !== null) return; // if ship spans across a cell that is not empty don't place it
+      if (gameBoardArray[y + ship.getLength() - 1] === undefined) return false; // if ship spans across a cell that is out of limits don't place it
+      if (gameBoardArray[y + ship.getLength() - 1][x] !== null) return false; // if ship spans across a cell that is not empty don't place it
       for (let i = 0; i < ship.getLength(); i += 1) {
         gameBoardArray[y + i].splice(x, 1, ship); // itterate through each row , replace element with an index of X by ship object
         placedShips.push(ship);
       }
-      return;
+      return true;
     }
 
-    if (gameBoardArray[y][x + ship.getLength() - 1] !== null) return; // if ship spans across a cell that is out of limits or contains another ship don't place it
+    if (gameBoardArray[y][x + ship.getLength() - 1] !== null) return false; // if ship spans across a cell that is out of limits or contains another ship don't place it
     for (let i = 0; i < ship.getLength(); i += 1) {
       gameBoardArray[y].splice(x + i, 1, ship); // select correct row, itterate through each cell changing the x index replacing each one by ship object
       placedShips.push(ship);
     }
+    return true;
   }
 
   function receiveAttack(y, x) {
