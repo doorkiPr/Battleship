e41f4cabf32235fec96efefbce04baae6598eb64
doorkiPr <boumarafchakib7@gmail.com>
2024-02-10 15:57:44 +0100

    Feat: add logic to set turns

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -1,13 +1,19 @@
-export default function Player(name, newGameboard) {
+export default function Player(name, newGameboard, turn = true) {
   const playerGameboard = newGameboard(name);
+  const isTurn = turn;
 
+  const getTurn = () => isTurn;
   const getName = () => name;
   const getGameboard = () => playerGameboard;
+  const toggleTurn = () => !isTurn;
+
   const attack = (enemyPlayer, y, x) => enemyPlayer.getGameboard().receiveAttack(y, x);
   const hasWon = (enemyPlayer) => enemyPlayer.getGameboard().areAllSunk();
   return {
     getName,
     getGameboard,
+    getTurn,
+    toggleTurn,
     attack,
     hasWon,
   };
