7e534f8e391aa8bdfdf775f69b3ad54dbd094f72
doorkiPr <boumarafchakib7@gmail.com>
2024-02-11 13:04:30 +0100

    Fix: fix eslint no return assignment issue

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -5,7 +5,10 @@ export default function Player(name, newGameboard, turn = true) {
   const getTurn = () => isTurn;
   const getName = () => name;
   const getGameboard = () => playerGameboard;
-  const toggleTurn = () => (isTurn = !isTurn);
+  const toggleTurn = () => {
+    isTurn = !isTurn;
+    return isTurn;
+  };
 
   const attack = (enemyPlayer, y, x) => enemyPlayer.getGameboard().receiveAttack(y, x);
   const hasWon = (enemyPlayer) => enemyPlayer.getGameboard().areAllSunk();
