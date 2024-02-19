b5bb23fb1685e5a6cce732114a730780b7c5f732
doorkiPr <boumarafchakib7@gmail.com>
2024-02-19 07:11:41 +0100

    Feat: add logic that dislays when ships are sunken

--- src/modules/RenderCell.js
+++ src/modules/RenderCell.js
@@ -4,7 +4,10 @@ export default function renderCell(gameBoardArray, i, j, player, enemy) {
   const cell = createHtmlElement("div", { id: `${i},${j}`, class: "cell" });
   if (gameBoardArray[i][j]) {
     if (gameBoardArray[i][j] === "missed") cell.textContent = "X";
-    if (gameBoardArray[i][j].isHit) cell.textContent = "hit";
+    if (gameBoardArray[i][j].isHit) {
+      if (gameBoardArray[i][j].ship.isSunk()) cell.textContent = "sunk";
+      else cell.textContent = "hit";
+    }
     if (gameBoardArray[i][j].getName) cell.textContent = gameBoardArray[i][j].getName();
   }
   cell.addEventListener("click", () => {
