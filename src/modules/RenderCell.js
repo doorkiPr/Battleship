e2869e3da041f2b695d2a4cbe44042438a0bb688
doorkiPr <boumarafchakib7@gmail.com>
2024-02-19 06:51:57 +0100

    Feat: add logic that attacks and change turns on click

--- src/modules/RenderCell.js
+++ src/modules/RenderCell.js
@@ -1,11 +1,19 @@
 import createHtmlElement from "../helperFunction/CreateHtmlElement";
 
-export default function renderCell(gameBoardArray, i, j) {
+export default function renderCell(gameBoardArray, i, j, player, enemy) {
   const cell = createHtmlElement("div", { id: `${i},${j}`, class: "cell" });
   if (gameBoardArray[i][j]) {
     if (gameBoardArray[i][j] === "missed") cell.textContent = "X";
     if (gameBoardArray[i][j].isHit) cell.textContent = "hit";
     if (gameBoardArray[i][j].getName) cell.textContent = gameBoardArray[i][j].getName();
   }
+  cell.addEventListener("click", () => {
+    if (!player.getTurn()) {
+      if (enemy.attack(player, ...cell.id.split(","))) {
+        player.toggleTurn();
+        enemy.toggleTurn();
+      }
+    }
+  });
   return cell;
 }
