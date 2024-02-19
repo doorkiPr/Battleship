d0de70907d345b9bfad584173371e8f2c7c287e9
doorkiPr <boumarafchakib7@gmail.com>
2024-02-19 06:50:15 +0100

    Feat: use renderCell function instead of creating an element , and rerender the gameboardArray on click

--- src/modules/RenderGameboard.js
+++ src/modules/RenderGameboard.js
@@ -1,12 +1,18 @@
-import createHtmlElement from "../helperFunction/CreateHtmlElement";
 // eslint-disable-next-line no-unused-vars
 import gameboardStyle from "../styles/gameboard.css";
+import renderCell from "./RenderCell";
 
-export default function renderGameboard(gameBoardArray, player) {
-  const gameboardContainer = document.querySelector(`#${player}`);
+export default function renderGameboard(player, playerNature, enemy) {
+  const gameboardContainer = document.querySelector(`#${playerNature}`);
+  const gameBoardArray = player.getGameboard().getGameboardArray();
+
   for (let i = 0; i < gameBoardArray.length; i += 1) {
-    for (let j = 0; j < gameBoardArray[i].length; j += 1) {
-      const cell = createHtmlElement("div", { id: `${i},${j}`, class: "cell" });
+    for (let j = 0; j < gameBoardArray.length; j += 1) {
+      const cell = renderCell(gameBoardArray, i, j, player, enemy);
+      cell.addEventListener("click", () => {
+        gameboardContainer.innerHTML = "";
+        renderGameboard(player, playerNature, enemy);
+      });
       gameboardContainer.appendChild(cell);
     }
   }
