dd8935fd95b26c95e81bdfcc35e154d101863f17
doorkiPr <boumarafchakib7@gmail.com>
2024-02-22 13:50:00 +0100

    Feat: add logic that displays whoever won the game

--- src/modules/RenderCell.js
+++ src/modules/RenderCell.js
@@ -1,4 +1,5 @@
 import createHtmlElement from "../helperFunction/CreateHtmlElement";
+import renderInformation from "./RenderInformation";
 
 export default function renderCell(gameBoardArray, i, j, player, enemy) {
   const cell = createHtmlElement("div", { id: `${i},${j}`, class: "cell" });
@@ -14,6 +15,8 @@ export default function renderCell(gameBoardArray, i, j, player, enemy) {
     if (player.getGameboard().areAllSunk() || enemy.getGameboard().areAllSunk()) return;
     if (!player.getTurn()) {
       if (enemy.attack(player, ...cell.id.split(","))) {
+        if (player.getGameboard().areAllSunk()) renderInformation("win", player, enemy);
+        if (enemy.getGameboard().areAllSunk()) renderInformation("win", enemy, player);
         player.toggleTurn();
         enemy.toggleTurn();
       }
