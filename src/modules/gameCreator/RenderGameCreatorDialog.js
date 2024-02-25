fb424d533f6dc5979955036c2a6d2c1c1bfef7b0
doorkiPr <boumarafchakib7@gmail.com>
2024-02-25 05:41:43 +0100

    Feat: add shipsArray and selected ship variable

--- src/modules/gameCreator/RenderGameCreatorDialog.js
+++ src/modules/gameCreator/RenderGameCreatorDialog.js
@@ -1,7 +1,17 @@
 export default function RenderGameCreatorDialog(Player, Gameboard) {
+  const shipsArray = [
+    { name: "Carrier", quantity: 1 },
+    { name: "Battleship", quantity: 1 },
+    { name: "Cruiser", quantity: 1 },
+    { name: "Submarine", quantity: 2 },
+    { name: "Destroyer", quantity: 3 },
+  ];
+  let selectedShip;
+
   const dialog = document.querySelector("#gameCreatorDialog");
   dialog.showModal();
   const form = document.querySelector("#gameCreatorForm");
+
   form.addEventListener("submit", () => {
     const name = document.getElementById("playerName").value;
     const newPlayer = Player(name, Gameboard);
