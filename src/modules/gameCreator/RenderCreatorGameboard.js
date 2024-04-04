44ada5d1f713245e14fed0972ace52ffa70ff1dd
doorkiPr <boumarafchakib7@gmail.com>
2024-04-04 03:47:40 +0200

    Feat: add logic to render ships and place the selected ship in the creator gameboard

--- src/modules/gameCreator/RenderCreatorGameboard.js
+++ src/modules/gameCreator/RenderCreatorGameboard.js
@@ -1,15 +1,21 @@
 // eslint-disable-next-line no-unused-vars
 import gameboardStyle from "../../styles/gameboard.css";
 import createHtmlElement from "../../helperFunction/CreateHtmlElement";
+import Ship from "../Ship";
 
-export default function RenderCreatorGameboard(player, shipsArray, selectedShip, dialog) {
+export default function RenderCreatorGameboard(player, shipsArray, getSelectedShip, dialog) {
   const gameboardContainer = createHtmlElement("div", { id: `creationGameboard`, class: "gameBoard" });
   const gameBoardArray = player.getGameboard().getGameboardArray();
 
   for (let i = 0; i < gameBoardArray.length; i += 1) {
     for (let j = 0; j < gameBoardArray.length; j += 1) {
       const cell = createHtmlElement("div", { id: `${i},${j}`, class: "cell" });
-      cell.addEventListener("click", () => {});
+      if (gameBoardArray[i][j]) cell.textContent = gameBoardArray[i][j].getName();
+      cell.addEventListener("click", () => {
+        player.getGameboard().placeShip(Ship(getSelectedShip()), i, j);
+        gameboardContainer.remove();
+        RenderCreatorGameboard(player, shipsArray, getSelectedShip, dialog);
+      });
       gameboardContainer.appendChild(cell);
     }
   }
