ffa4e2b52ccc1393da6f198440f9dac89af58980
doorkiPr <boumarafchakib7@gmail.com>
2024-05-03 07:29:22 +0200

    Feat: initialize game

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-console.log("hello world");
+import RenderGamemodeDialog from "./modules/gameCreator/RenderGamemodeDialog";
+// eslint-disable-next-line no-unused-vars
+import style from "./styles.css";
+
+RenderGamemodeDialog();
