f3539a81c7c052e5532f3cfdaf9d19ea7b2047a9
doorkiPr <boumarafchakib7@gmail.com>
2024-05-04 15:16:04 +0200

    Feat: add logic to change class of cell instead of text content depending on the type of ship

--- src/modules/RenderCell.js
+++ src/modules/RenderCell.js
@@ -1,24 +1,33 @@
 import createHtmlElement from "../helperFunction/CreateHtmlElement";
 import computerAI from "./ComputerAI";
 import renderInformation from "./RenderInformation";
+// eslint-disable-next-line no-unused-vars
+import style from "../styles/cell.css";
 
 export default function renderCell(gameBoardArray, i, j, player, enemy, renderGameboard) {
   const cell = createHtmlElement("div", { id: `${i},${j}`, class: "cell" });
   if (gameBoardArray[i][j]) {
     if (gameBoardArray[i][j] === "missed") cell.textContent = "X";
+
     if (gameBoardArray[i][j].isHit) {
-      if (gameBoardArray[i][j].ship.isSunk()) cell.textContent = "sunk";
-      else cell.textContent = "hit";
+      cell.classList.add("ship");
+
+      if (gameBoardArray[i][j].ship.isSunk()) {
+        cell.textContent = "sunk";
+        cell.classList.add(gameBoardArray[i][j].ship.getName().toLowerCase());
+      } else cell.textContent = "hit";
     }
+
     if (gameBoardArray[i][j].getName && player.getNature() === "human")
-      cell.textContent = gameBoardArray[i][j].getName();
+      cell.classList.add(gameBoardArray[i][j].getName().toLowerCase(), "ship");
   }
+
   cell.addEventListener("click", () => {
     if (player.getGameboard().areAllSunk() || enemy.getGameboard().areAllSunk()) return;
 
     if (enemy.getTurn()) {
       if (enemy.attack(player, ...cell.id.split(","))) {
-        renderInformation("turn", player);
+        renderInformation("turn", player, enemy);
 
         if (player.getGameboard().areAllSunk()) renderInformation("win", player, enemy);
         if (enemy.getGameboard().areAllSunk()) renderInformation("win", enemy, player);
