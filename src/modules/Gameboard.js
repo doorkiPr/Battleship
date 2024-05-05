3596960034e68d394cf83ac2d8d4636d6184498b
doorkiPr <boumarafchakib7@gmail.com>
2024-05-05 14:00:11 +0200

    Fix: fix bug by checking each cell instead of only the last one when placing a ship to avoid overlap

--- src/modules/Gameboard.js
+++ src/modules/Gameboard.js
@@ -8,12 +8,18 @@ export default function Gameboard(name) {
   }
   const getName = () => name.toString();
   const getGameboardArray = () => gameBoardArray;
+
   function placeShip(ship, y, x, axis = "horizontal") {
     if (y < 0 || y > 10 || x < 0 || x > 10) return false;
+
     if (gameBoardArray[y][x]) return false;
+
     if (axis === "vertical") {
-      if (gameBoardArray[y + ship.getLength() - 1] === undefined) return false; // if ship spans across a cell that is out of limits don't place it
-      if (gameBoardArray[y + ship.getLength() - 1][x] !== null) return false; // if ship spans across a cell that is not empty don't place it
+      for (let i = 0; i < ship.getLength(); i += 1) {
+        if (gameBoardArray[y + i] === undefined || gameBoardArray[y + i][x] !== null) {
+          return false; //  if any cell is already occupied by another ship or if ship is placed out of bounds, return false
+        }
+      }
       for (let i = 0; i < ship.getLength(); i += 1) {
         gameBoardArray[y + i].splice(x, 1, ship); // itterate through each row , replace element with an index of X by ship object
         placedShips.push(ship);
@@ -21,7 +27,12 @@ export default function Gameboard(name) {
       return true;
     }
 
-    if (gameBoardArray[y][x + ship.getLength() - 1] !== null) return false; // if ship spans across a cell that is out of limits or contains another ship don't place it
+    for (let i = 0; i < ship.getLength(); i += 1) {
+      if (gameBoardArray[y + i] === undefined || gameBoardArray[y][x + i] !== null) {
+        return false; // if any cell is already occupied by another ship or if ship is placed out of bounds, return false
+      }
+    }
+
     for (let i = 0; i < ship.getLength(); i += 1) {
       gameBoardArray[y].splice(x + i, 1, ship); // select correct row, itterate through each cell changing the x index replacing each one by ship object
       placedShips.push(ship);
