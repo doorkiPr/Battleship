a7240ad9aa85e8160a6f292d4832bcf5b305444d
doorkiPr <boumarafchakib7@gmail.com>
2024-05-05 08:15:23 +0200

    Fix:change variable name to be more accurate

--- src/helperFunction/generateIcon.js
+++ src/helperFunction/generateIcon.js
@@ -1,13 +1,13 @@
 import createHtmlElement from "./CreateHtmlElement";
 
 export default function GenerateIcon() {
-  const destruction = createHtmlElement("span", { class: "material-symbols-outlined" });
-  destruction.textContent = "destruction";
+  const destroyed = createHtmlElement("span", { class: "material-symbols-outlined" });
+  destroyed.textContent = "skull";
 
   const fire = createHtmlElement("span", { class: "material-symbols-outlined" });
   fire.textContent = "local_fire_department";
 
   const missed = createHtmlElement("span", { class: "material-symbols-outlined" });
   missed.textContent = "close";
-  return { destruction, fire, missed };
+  return { destroyed, fire, missed };
 }
