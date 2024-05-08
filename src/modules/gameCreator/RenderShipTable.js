be6ae30f80d200179c72e7226c49ad45a991d262
doorkiPr <boumarafchakib7@gmail.com>
2024-05-08 07:05:18 +0200

    Refractor: add spacing

--- src/modules/gameCreator/RenderShipTable.js
+++ src/modules/gameCreator/RenderShipTable.js
@@ -5,15 +5,20 @@ export default function renderShipTable(shipsArray, updateSelectedShip, wrapper)
   const shipTable = createHtmlElement("div", { id: "shipTable" });
   shipsArray.forEach((ship) => {
     const shipWrapper = createHtmlElement("div", { id: `${ship.name}wrapper`, class: "shipWrapper" });
+
     const newShip = createHtmlElement("div", { id: ship.name, class: "ship" });
-    const quantity = createHtmlElement("div", { id: ship.name, class: "shipQuantity" });
     newShip.textContent = ship.name;
+
+    const quantity = createHtmlElement("div", { id: ship.name, class: "shipQuantity" });
     quantity.textContent = ship.quantity;
+
     newShip.addEventListener("click", () => {
       if (ship.quantity) updateSelectedShip(ship.name);
     });
+
     shipWrapper.appendChild(newShip);
     shipWrapper.appendChild(quantity);
+
     shipTable.appendChild(shipWrapper);
   });
   wrapper.appendChild(shipTable);
